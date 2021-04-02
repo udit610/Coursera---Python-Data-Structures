3fff5c08e41cc78d8141b3066118f1c6f3c71721
udit610 <74165951+udit610@users.noreply.github.com>
2021-04-02 15:42:54 +0530

    Update Assignment8.4.py

--- Week-4/Assignment8.4.py
+++ Week-4/Assignment8.4.py
@@ -12,18 +12,8 @@ lst = list()
 count=0
 for line in fh:
      line = line.rstrip()
-    
      for word in line.split():
-        
         if word not in lst:
-            
             lst.append(word)
-            
-           
-
-	
-
-
 lst.sort()
-print lst
-
+print(lst)
